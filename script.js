2b77eae1870cdb9576190a76bf7da3b788b99d59
AlanMortellaro <alan.mortellaro@hotmail.com>
2017-05-19 11:48:42 +0200

    Ajout txtbox pour token

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ $(function() {
   $("#send").click(function() {
     let id = $("#NoID").val()
     let message = $("#bodyMessage").val()
+    let token = $("#token").val()
     //$("#NoID").val("")
     $("#bodyMessage").val("")
     $('body').append("<div>" + id + "</div>")
     $('body').append("<div>" + message + "</div>")
 
     $.ajax({
-      url: 'https://api.telegram.org/bot314712956:AAFw-yqfam3M2dlDp50H9vJXx0wBgiGVYyA/sendMessage?chat_id=' + id + '&text=' + message
+      url: 'https://api.telegram.org/bot' + token + '/sendMessage?chat_id=' + id + '&text=' + message
     });
   })     
 
